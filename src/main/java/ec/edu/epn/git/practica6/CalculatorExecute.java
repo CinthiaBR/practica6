d5e5324f7d42c994dde1c41873df94a4898998a1
CinthiaBR <cintha.burbanopn.edu.ec>
2021-01-17 00:52:23 -0500

    adding calculator Execute

---
+++ src/main/java/ec/edu/epn/git/practica6/CalculatorExecute.java
@@ -0,0 +1,16 @@
+package ec.edu.epn.git.practica6;
+
+import java.sql.SQLOutput;
+
+public class CalculatorExecute {
+    public static void main (String[] args){
+        System.out.println("==Calculator Execute ==");
+        Calculator c = new Calculator();
+        int addition =c.addition(4,7);
+        System.out.println("c.addition(4,7)= "+ addition);
+        int substraction =c.subtraction(7,1);
+        System.out.println("c.substraction(7,1)= "+ substraction);
+        int division =c.division(7,7);
+        System.out.println("c.division(7,7)= "+ division);
+    }
+}
