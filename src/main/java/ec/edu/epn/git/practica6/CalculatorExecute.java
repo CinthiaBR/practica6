f4890a1989dd5c060b7a8dd033c351344534ccbf
CinthiaBR <cintha.burbanopn.edu.ec>
2021-01-17 13:39:10 -0500

    actualizaciones

--- src/main/java/ec/edu/epn/git/practica6/CalculatorExecute.java
+++ src/main/java/ec/edu/epn/git/practica6/CalculatorExecute.java
@@ -6,11 +6,13 @@ public class CalculatorExecute {
     public static void main (String[] args){
         System.out.println("==Calculator Execute ==");
         Calculator c = new Calculator();
-        int addition =c.addition(4,7);
-        System.out.println("c.addition(4,7)= "+ addition);
+        int addition =c.addition(7,7);
+        System.out.println("c.addition(7,7)= "+ addition);
         int substraction =c.subtraction(7,1);
         System.out.println("c.substraction(7,1)= "+ substraction);
         int division =c.division(7,7);
         System.out.println("c.division(7,7)= "+ division);
+        int multiplication = c.multiplication(6,2);
+        System.out.println("c.multiplication (6,2) = " + multiplication);
     }
 }
